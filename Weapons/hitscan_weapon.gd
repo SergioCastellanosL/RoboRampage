eb51343f5ab4c55704739f1cbf2f9131c1b3220c
Sergio Andrés Castellanos Landazábal <serancaslan@gmail.com>
2025-08-20 15:47:49 -0400

    weapon parrticles

--- Weapons/hitscan_weapon.gd
+++ Weapons/hitscan_weapon.gd
@@ -4,6 +4,7 @@ extends Node3D
 @export var recoil := 0.05
 @export var weapon_mesh : Node3D
 @export var weapon_damage := 15
+@export var muzzle_flash : GPUParticles3D 
 @onready var cooldown_timer: Timer = $CooldownTimer
 @onready var weapon_position : Vector3 = weapon_mesh.position
 @onready var ray_cast_3d: RayCast3D = $RayCast3D
@@ -19,6 +20,7 @@ func _process(delta: float) -> void:
 			shoot()
 	weapon_mesh.position = weapon_mesh.position.lerp(weapon_position,delta*10.0)
 func shoot() -> void:
+	muzzle_flash.restart()
 	cooldown_timer.start(1.0/fire_rate)
 	var collider = ray_cast_3d.get_collider()
 	weapon_mesh.position.z += recoil
